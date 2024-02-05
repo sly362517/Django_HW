7d4d08589e0b9961c7e27b0925c09354450929cd
sly362517 <machortovdima@mail.ru>
2024-02-05 13:56:26 +0300

    added HW1!

--- HW_1/about_me_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('main/', views.main, name='Главная страница'),
-    path('about_me/', views.about_me, name='О себе'),
-
-]
--- Seminar/internet_shop/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InternetShopConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'internet_shop'
